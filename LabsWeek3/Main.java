104788a978d640b5178d5fa98e4e7fc1b3b1ee33
Benjamin Szasz CSerei <84959986+BenjaminSzasz@users.noreply.github.com>
2024-05-02 21:53:34 +0200

    :tada:Task 1 done Might update it later

--- LabsWeek3/Main.java
+++ LabsWeek3/Main.java
@@ -15,10 +15,10 @@ public class Main {
         //and it rounds the number to one decimal.
 
         MathContext mc = new MathContext(2);
-        BigDecimal bd1 = new BigDecimal(1.2345);
+        BigDecimal bd1 = BigDecimal.valueOf(1.2345);
         System.out.println(bd1.negate(mc));//This method returns the negative value
         MathContext mc1 =new MathContext(3);
-        BigDecimal bd2 = new BigDecimal(-45.67);
+        BigDecimal bd2 = BigDecimal.valueOf(-45.67);
         System.out.println(bd2.abs(mc1));//This method returns the absolute value
 
 
